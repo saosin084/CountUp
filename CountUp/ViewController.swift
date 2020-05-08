92c6e3d54a063ff6db3239f60c4501fdec452e05
Yuta <yuta5196@gmail.com>
2020-05-08 22:40:27 +0900

    count up and count down

--- CountUp/ViewController.swift
+++ CountUp/ViewController.swift
@@ -1,20 +1,46 @@
-//
-//  ViewController.swift
-//  CountUp
-//
-//  Created by yuta on 2020/05/06.
-//  Copyright © 2020 yuta. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var countuplabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countuplabel.text = String(count)
     }
+    
+    @IBAction func plus(_ sender: Any){
+        
+        count = count + 1
+        countuplabel.text = String(count)
+        
+        if count >= 10{
+            changecolor()
+        }
+    }
+    
+    @IBAction func minus(_ sender: Any) {
 
+        count = count - 1
+        countuplabel.text = String(count)
+        
+        if count <= 0{
+            resetcolor()
+        }
+    }
+    
+    func changecolor(){
 
+        countuplabel.textColor = .yellow
+    }
+    
+    func resetcolor(){
+
+        countuplabel.textColor = .white
+    }
 }
 
